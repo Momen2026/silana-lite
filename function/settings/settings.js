6d8845100825c55320a5051d3ec0769273c42585
Momen2026 <ssttyyii44@gmail.com>
2025-03-11 02:28:34 +0200

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '249120319899',
+ pairingNumber : '249120319899',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '249120319899',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '249120319899',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
